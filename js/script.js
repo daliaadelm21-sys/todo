f0b0d11708bd95390e7773a41a88bfc083fbc501
Moaz Ahmed <moazahmed.official0@gmail.com>
2026-04-25 17:35:07 +0300

    Refactor addTask function for improved readability and consistency

--- js/script.js
+++ js/script.js
@@ -3,39 +3,40 @@ const input = document.querySelector("input");
 const warning = document.querySelector(".warning");
 const tasks = document.querySelector(".lists-container");
 
-button.addEventListener("click",addTask);
-function addTask(){
-    console.log("clicked")
-    let task_text = input.value;
-    if(task_text == "" || task_text.trim() == ""){
+button.addEventListener("click", addTask);
+function addTask() {
+    const task_text = input.value.trim();
+    if (task_text === "") {
         warning.style.display = "block";
     }
-    else{
-        let task = document.createElement("li");
+    else {
+        const task = document.createElement("li");
         task.textContent = task_text;
         tasks.appendChild(task);
         warning.style.display = "none";
-        let delete_btn = document.createElement("span");
-delete_btn.textContent = "X";
-task.appendChild(delete_btn);
-saveTask ()
+        const delete_btn = document.createElement("span");
+        delete_btn.textContent = "X";
+        task.appendChild(delete_btn);
+        saveTask();
+        input.value = "";
+        input.focus();
     }
 }
 
-tasks.addEventListener("click", function(event){
-    if(event.target.tagName == "LI"){
+tasks.addEventListener("click", function (event) {
+    if (event.target.tagName == "LI") {
         event.target.classList.toggle("checked");
-        saveTask ()
-    }else if(event.target.tagName == "SPAN"){
+        saveTask()
+    } else if (event.target.tagName == "SPAN") {
         event.target.parentElement.remove();
-        saveTask ()
+        saveTask()
     }
-    
+
 });
-function saveTask (){
+function saveTask() {
     localStorage.setItem("tasks", tasks.innerHTML)
 }
-function getTasks(){
+function getTasks() {
     tasks.innerHTML = localStorage.getItem("tasks");
 }
 getTasks()
